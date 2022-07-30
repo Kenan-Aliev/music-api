fe30c85ba871ecc463607be7899d2af99c2eafc8
Kenan-Aliev <kenan199948@gmail.com>
2022-07-30 11:27:55 +0600

    make it better

--- db.js
+++ db.js
@@ -1,20 +1,25 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize({
-  database: process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+const sequelize = new Sequelize(
+  `${process.env.DATABASE_NAME}`,
+  `${process.env.DATABASE_USERNAME}`,
+  `${process.env.DATABASE_PASSWORD}`,
+  {
+    // database: process.env.DATABASE_NAME,
+    // username: process.env.DATABASE_USERNAME,
+    // password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    // port: process.env.DATABASE_PORT,
+    dialect: "postgres",
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false,
+    //   },
+    // },
+    define: {
+      timestamps: false,
     },
-  },
-  define: {
-    timestamps: false,
-  },
-});
+  }
+);
 
 module.exports = sequelize;
